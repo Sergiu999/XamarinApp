1670a160d3edefab0d5cc4560b21913ec4af2c5d
Sergiu <sergiu.sau@sapec.md>
2018-02-28 13:45:18 +0200

    Initial Commit

---
+++ XamarinApp/XamarinApp/GreetPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GreetPage : ContentPage
+    {
+        public GreetPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Handle_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Title", "Hello World", "OK");
+        }
+    }
+}
